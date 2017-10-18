cde7b394db2288a5620ebc3c5d06a1b990a4874f
Alperen Köylü <alperenkoylu@gmail.com>
2017-10-18 15:42:02 +0300

    Create main.java

---
+++ ninjaFight/main.java
@@ -0,0 +1,10 @@
+public static void main(String[] args) {
+		
+		ninja nj1 = new ninja();
+		nj1.setNinja("alperen");
+
+		ninja nj2 = new ninja();
+		nj2.setNinja("mustafa");
+
+		arena.fight(nj1, nj2);
+	}
